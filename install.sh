ff70390b6b4ff3a7344578f02a2ef2f7f3960a39
Brooks Patton <brooks@brookzerker.com>
2019-01-29 22:04:53 -0700

    script detects if we are using a mac and will install homebrew

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This install script will determine what type of computer we are running on and then run the appropriate install scripts to get everything (or at least as much as possible set up
+
+function log() {
+  echo "*************************"
+  echo "$1"
+  echo "*************************"
+}
+
+function mac_install() {
+  log "installing homebrew"
+  bash -c ./homebrew.sh
+
+  # zsh
+  # .zshrc
+}
+
+case "$(uname -s)" in
+  Darwin*) OPERATING_SYSTEM="mac";;
+  *)       OPERATING_SYSTEM="unknown"
+esac
+
+if [ "$OPERATING_SYSTEM" == "mac" ]
+then
+  mac_install
+else
+  echo unknown operating system, please check uname
+fi
