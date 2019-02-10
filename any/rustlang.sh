e475d32ed531b0cd6f04773297e9d1a32c48660e
Brooks Patton <brooks@brookzerker.com>
2019-02-10 14:45:54 -0700

    added rust

---
+++ any/rustlang.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl https://sh.rustup.rs -sSf | sh
+
+echo 
+echo "** Rust-lang installed"
+echo
---
+++ mac/ggez.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brew install sdl2
+
+echo
+echo "** libraries installed for ggez"
+echo
