f951c1a70413444bc179006156a5be49a0a0decd
Brooks Patton <brooks@brookzerker.com>
2019-02-11 15:57:29 -0700

    setting up vscode and some fonts

---
+++ mac/fonts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew cask install font-hack-nerd-font
+brew cask install font-firacode
+
+echo
+echo "** fonts installed"
+echo
---
+++ mac/vscode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brew cask install visual-studio-code
+
+echo
+echo "** visual studio code installed"
+echo
